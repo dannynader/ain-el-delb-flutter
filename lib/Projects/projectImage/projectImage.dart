cc909fe8f764638007ac1022176a84a82614837d
danny <>
2022-02-18 18:57:14 +0200

    pics project

--- lib/Projects/projectImage/projectImage.dart
+++ lib/Projects/projectImage/projectImage.dart
@@ -1,9 +1,7 @@
 import 'package:aineldelb/component/NavBar.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
 import 'package:galleryimage/galleryimage.dart';
-
 import "package:http/http.dart" as http;
 import 'dart:convert';
 
@@ -15,35 +13,27 @@ class ProjectImage extends StatefulWidget {
 }
 
 class _ProjectImageState extends State<ProjectImage> {
-  late List<String> urls = [];
-  late List<String> imageUrl = [];
-  List imagesWidget = [];
-  List<String> listOfUrls = [
-    "https://cosmosmagazine.com/wp-content/uploads/2020/02/191010_nature.jpg",
-    "https://scx2.b-cdn.net/gfx/news/hires/2019/2-nature.jpg",
-    "https://isha.sadhguru.org/blog/wp-content/uploads/2016/05/natures-temples.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/7/77/Big_Nature_%28155420955%29.jpeg",
-    "https://s23574.pcdn.co/wp-content/uploads/Singular-1140x703.jpg",
-    "https://www.expatica.com/app/uploads/sites/9/2017/06/Lake-Oeschinen-1200x675.jpg",
-    "https://www.expatica.com/app/uploads/sites/9/2017/06/Lake-Oeschinen-1200x675.jpg",
-    "https://www.expatica.com/app/uploads/sites/9/2017/06/Lake-Oeschinen-1200x675.jpg",
-    "https://www.expatica.com/app/uploads/sites/9/2017/06/Lake-Oeschinen-1200x675.jpg",
-  ];
-  getProjectImages() async {
-    final response = await http.get(
+  List<String> posts = [];
+  List<String> temPosts = [];
+  late List<dynamic> jsonData;
+  int index = 0;
+  Future<List> getProjectImages() async {
+    final http.Response response = await http.get(
         Uri.parse('https://api.aineldelb.gov.lb/api/projects-albumsV2/40'));
-    final jsonData = jsonDecode(response.body) as List;
+    jsonData = jsonDecode(response.body);
+    makeList();
+    return jsonData;
+  }
 
-    jsonData.map((e) => imageUrl
-        .add('https://api.aineldelb.gov.lb/storage/' + e['project']) as List);
-
-    if (kDebugMode) {
-      print(imageUrl);
+  makeList() {
+    for (index = 0; index < jsonData.length; index++) {
+      if (kDebugMode) {
+        print(jsonData[index]['project']);
+      }
+      posts.add(jsonData[index]['project']);
     }
-
     setState(() {
-      imagesWidget = jsonData;
-      urls = imageUrl;
+      posts = posts;
     });
   }
 
@@ -61,9 +51,21 @@ class _ProjectImageState extends State<ProjectImage> {
           backgroundColor: const Color.fromRGBO(103, 0, 103, 1),
           title: const Text("مشاريع"),
         ),
-        body: urls.isEmpty
+        body: posts.isNotEmpty
             ? SingleChildScrollView(
-                child: GalleryImage(titleGallery: 'hi', imageUrls: listOfUrls))
+                child: GalleryImage(titleGallery: 'hi', imageUrls: posts))
             : const Text('data'));
+  }
+}
+
+class ProjectModel {
+  final String project;
+
+  ProjectModel({required this.project});
+
+  factory ProjectModel.fromJson(Map<String, dynamic> json) {
+    return ProjectModel(
+      project: json['project'],
+    );
   }
 }
